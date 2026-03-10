9eb651c3ac81ce543bae9e4aa4410f274f45ec94
mesyaministry-sys <mesyaministry@gmail.com>
2026-03-10 10:23:57 +0700

    Update app_baru.py

--- app_baru.py
+++ app_baru.py
@@ -183,7 +183,7 @@ with col_logo:
 with col_teks:
     st.markdown('<div class="main-header">FACTORY OPERATION DASHBOARD</div>', unsafe_allow_html=True)
     st.markdown(f'<div class="sub-header"> <b>{sheet_name}</b> | Production, Quality & Efficiency</div>', unsafe_allow_html=True)
-    st.markdown('<div class="dev-credit">✨ Created & Dev : Mahesya</div>', unsafe_allow_html=True)
+    st.markdown('<div class="dev-credit">✨ Dev : Mahesya</div>', unsafe_allow_html=True)
 st.markdown('</div>', unsafe_allow_html=True)
 st.divider()
 
@@ -503,6 +503,7 @@ if sheet_id:
     else:
         st.dataframe(df_display, use_container_width=True)
         st.caption("Data belum tersedia. Tabel di atas adalah template kolom yang akan diisi.")
+
 
 
 
